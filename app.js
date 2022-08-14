62c634e567e6bc44d6e7d86b8babfdab17261248
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-08-14 14:21:38 -0500

    Feat: Added OS module

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-// Modulo process
+// Modulo OS
 // Autor: @sebastiancb29
-// Provee información sobre el proceso actual de NodeJS que se está ejecutando.
+// Contiene funcionalidad para obtener información sobre el sistema operativo en el cual se ejecuta la aplicación.
 
-console.log(process.env) // Muestra en la consola la información de las variables de entorno
+const os = require('os');
 
-// node app.js 6 7 
-// [node, app.js, 6, 7]
-//    0    1      2  3
+console.log(os.type()); // Muestra en la consola el tipo de sistema operativo
 
-console.log(process.argv); // Muestra en la consola la información de los argumentos de la línea de comandos
-console.log(process.argv[2]);
-console.log(process.argv[3]);
-
-// Recorrer todos los argumentos de la línea de comandos
-for(let i = 2; i < process.argv.length; i++) {
-    console.log(process.argv[i]);
-}
-
-console.log(process.memoryUsage()); // Muestra en la consola la información de la memoria
+console.log(os.homedir()); // Muestra en la consola la ruta de la carpeta home del usuario
+console.log(os.uptime()); // Muestra en la consola el tiempo de actividad del sistema
+console.log(os.userInfo()); // Muestra en la consola la información del usuario
